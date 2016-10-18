52e159e76c354d5a33cb51109a7b0189712f4c55
SimulationServe\SimulationServerPC <red96146@naver.com>
2016-10-18 11:00:45 +0900

    2016.10.18 프로젝트 병합

---
+++ Assets/Script/DataStructure/FurnitureData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+
+[System.Serializable]
+public class FurnitureData
+{
+	// field
+	[SerializeField] string id;
+	[SerializeField] Vector3 position;
+	[SerializeField] int height;
+	[SerializeField] int widthX;
+	[SerializeField] int widthZ;
+	[SerializeField] bool isAllocated;
+	[SerializeField] FunctionType functionType;
+	[SerializeField] AllocateType allocateType;
+
+	// property
+	public Vector3 Position { get { return position; } set { position = value; } }
+
+	public int Height { get { return height; } }
+
+	public int WidthX { get { return widthX; } }
+
+	public int WidthZ { get { return widthZ; } }
+
+	public FunctionType Function { get { return functionType; } }
+
+	public AllocateType Allocate { get { return allocateType; } }
+
+	// enum type
+	// function type
+	public enum FunctionType : int
+	{
+		Default = 0,
+		CreateObject = 1,
+		SellObject = 2,
+		DecorateObject = 3,
+		StorageObject = 4}
+
+	;
+
+	// allocate type
+	public enum AllocateType : int
+	{
+		Default = 0,
+		Field = 1,
+		Wall = 2,
+		Wherever = 3}
+
+	;
+
+	// constructor - no parameter -> set default;
+	public FurnitureData()
+	{
+		id = null;
+		position = Vector3.zero;
+		height = 0;
+		widthX = 0;
+		widthZ = 0;
+		isAllocated = false;
+		functionType = FunctionType.Default;
+		allocateType = AllocateType.Default;
+	}
+
+	// constructor - all parameter -> set up data
+	public FurnitureData( string _id, Vector3 _position, int _height, int _widthX, int _widthZ, bool _isAllocated, FunctionType _functionType, AllocateType _allocateType )
+	{
+		id = _id;
+		position = _position;
+		height = _height;
+		widthX = _widthX;
+		widthZ = _widthZ;
+		isAllocated = _isAllocated;
+		functionType = _functionType;
+		allocateType = _allocateType;
+	}
+
+	// constructor - data parameter -> make same data instance
+	public FurnitureData( FurnitureData data )
+	{
+		id = data.id;
+		position = data.position;
+		height = data.height;
+		widthX = data.widthX;
+		widthZ = data.widthZ;
+		isAllocated = data.isAllocated;
+		functionType = data.functionType;
+		allocateType = data.allocateType;
+	}
+}
---
+++ Assets/Script/TileMap.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent( typeof( MeshFilter ) )]
+[RequireComponent( typeof( MeshRenderer ) )]
+[RequireComponent( typeof( MeshCollider ) )]                
+
+public class TileMap : MonoBehaviour
+{
+	// field
+	[SerializeField] int sizeX = 10;
+	[SerializeField] int sizeY = 10;
+	[SerializeField] float tileSize = 1.0f;
+	[SerializeField] Texture2D reticleLine;
+	[SerializeField] int tileResoultion;
+	// property
+	public float TileSize { get { return tileSize; } }
+
+	// Use this for initialization
+	void Start()
+	{
+		BuildMesh();
+	}
+
+	// public method
+	public void BuildMesh()
+	{
+		int tileNumber = sizeX * sizeY;
+		int triangleNumber = tileNumber * 2;
+		int vertexSizeX = sizeX + 1;
+		int vertexSizeY = sizeY + 1;
+		int vertexNumber = vertexSizeX * vertexSizeY;
+		
+		// generate mesh data
+		Vector3[] vertices = new Vector3[vertexNumber];
+		Vector3[] normals = new Vector3[vertexNumber];
+		Vector2[] uv = new Vector2[vertexNumber];
+
+		int[] triangles = new int[triangleNumber * 3];
+
+		int x, y;
+		for( y = 0; y < vertexSizeY; y++ )
+		{
+			for( x = 0; x < vertexSizeX; x++ )
+			{
+				vertices[ y * vertexSizeX + x ] = new Vector3( x * tileSize, 0, y * tileSize );
+				normals[ y * vertexSizeX + x ] = Vector3.up;
+				uv[ y * vertexSizeX + x ] = new Vector2( ( float ) x / vertexSizeX, ( float ) y / vertexSizeY );
+			}
+		}
+
+		for( y = 0; y < sizeY; y++ )
+		{
+			for( x = 0; x < sizeX; x++ )
+			{
+				int squareIndex = y * sizeX + x;
+				int triangleOffset = squareIndex * 6;
+				triangles[ triangleOffset + 0 ] = y * vertexSizeX + x + 0;
+				triangles[ triangleOffset + 2 ] = y * vertexSizeX + x + vertexSizeX + 1;
+				triangles[ triangleOffset + 1 ] = y * vertexSizeX + x + vertexSizeX + 0;
+
+				triangles[ triangleOffset + 3 ] = y * vertexSizeX + x + 0;
+				triangles[ triangleOffset + 5 ] = y * vertexSizeX + x + 1;
+				triangles[ triangleOffset + 4 ] = y * vertexSizeX + x + vertexSizeX + 1;
+			}
+		}
+
+		// create a new mesh & populate with the data
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.normals = normals;
+		mesh.uv = uv;
+
+		// assign our mesh to our filter / rendeerer / collider
+		MeshFilter meshFilter = GetComponent<MeshFilter>();
+		MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+		MeshCollider meshCollider = GetComponent<MeshCollider>(); 
+
+		meshCollider.sharedMesh = mesh;
+		meshFilter.mesh = mesh;
+	}
+
+}
